dfcd07c7218aeac44bc75311908acf56860f5838
Mikk0git <104311953+Mikk0git@users.noreply.github.com>
2023-12-07 13:27:53 +0100

    Update szyfrCezara.py

--- szyfrCezara/szyfrCezara.py
+++ szyfrCezara/szyfrCezara.py
@@ -2,7 +2,10 @@ text = input("Podaj tekst: ")
 key = int(input("Podaj klucz: ")) 
 encryptedText = "" 
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
+
+alphabetCapital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] 
+
 
 for letter in text: 
     # print(letter) 
@@ -14,7 +17,15 @@ for letter in text:
             encryptedIndex = encryptedIndex - len(alphabet)
         # print(encryptedIndex)
         encryptedLetter = alphabet[encryptedIndex] 
-        encryptedText = encryptedText + encryptedLetter 
+        encryptedText = encryptedText + encryptedLetter
+        
+    elif letter in alphabetCapital:
+        encryptedIndex = index + key
+        while encryptedIndex > len(alphabetCapital):
+            encryptedIndex = encryptedIndex - len(alphabetCapital)
+        # print(encryptedIndex)
+        encryptedLetter = alphabetCapital[encryptedIndex] 
+        encryptedText = encryptedText + encryptedLetter
     else:  
         encryptedText = encryptedText + letter 
 
